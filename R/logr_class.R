d28be4d31d3a33ae2973e97f703f1a93335df1cb
Huashan <chenhuashan@gmail.com>
2017-11-08 13:51:34 +0800

    multiple changes
    + property withPID
    + property withTime
    * Resolve multithread conflicts
    * code refactor

--- R/logr_class.R
+++ R/logr_class.R
@@ -23,22 +23,30 @@
 #' log$loginterval(TRUE)
 #' Sys.sleep(3)
 #' log$loginterval('done!')
+#' log$quit()
 #' rm(log); gc(TRUE)
+#' 
 #' 
 #' demo for multithread computing
 #' source("e:/Huashan/Stat/R/packages/textnet/R/mc_utils.R", chdir = T, encoding = 'UTF-8')
 #' do_thread <- function(x, loger) {
-#'   loger$loginfoT(x)
+#'   #loger$loginfoT(paste(x, Sys.getpid()))
+#'   loger$loginterval(TRUE)
+#'   Sys.sleep(sample(5))
+#'   loger$loginterval(x)
 #' }
 #' loger <- logr$new()
 #' mcStart()
 #' foreach(j = 1:6, .packages = 'logr', .export = c('do_thread')) %dopar% do_thread(j, loger)
 #' mcEnd()
+#' loger$quit()
 #' rm(loger);gc(T)
 #' 
 #' logr$new('192.168.1.3', port = 9999, logfile = 'log.txt')
 logr <- R6Class("logr",
                     public = list(
+                      withPID = TRUE,
+                      withTime = TRUE,
                       initialize = function(host = 'localhost', port = 5555, logfile) {
                         if (Sys.info()['sysname'] == 'Windows') {
                           nc <- system.file('exe/nc.exe', package = 'logr')
@@ -49,7 +57,10 @@ logr <- R6Class("logr",
                         }
                         if (!missing(logfile)) {
                           dir.create(dirname(logfile), recursive = T, showWarnings = F)
-                          cmd <- sprintf('%s > %s', cmd, shQuote(logfile))
+                          
+                          # Linux 下使用文件管道，Windows下使用参数 -o
+                          fo <- if (Sys.info()['sysname'] == 'Windows') '-o' else '>>'
+                          cmd <- sprintf('%s %s %s', cmd, fo, shQuote(logfile))
                         }
                         system(cmd, wait = FALSE,
                                show.output.on.console = FALSE, invisible = FALSE)
@@ -59,28 +70,20 @@ logr <- R6Class("logr",
                         if (!is.null(private$log.socket)) close.socket(private$log.socket)
                       },
                       loginfo = function(msg) {
-                        msg <- as.character(msg)
-                        write.socket(private$log.socket, msg)
-                        write.socket(private$log.socket, '\n')
+                        private$.msg(msg)
                       },
                       #' log info with time stamp
                       loginfoT = function(msg) {
-                        msg <- as.character(msg)
-                        msg <- sprintf('%s: %s\n', as.character(Sys.time()), msg)
-                        write.socket(private$log.socket, msg)
+                        private$.msg(msg, TRUE)
                       },
                       #' log info with time interval
-                      #' @param start set TRUE to begin interval counting
+                      #' @param msg string or logic. Set msg to TRUE to begin interval counting,
+                      #'            FALSE or string to stop interval counting.
                       loginterval = function(msg) {
-                        #if (isTRUE(start)) {
-                        if (is.logical(msg)) {
-                          private$saved.tick <- as.character(Sys.time())
+                        if (is.logical(msg) & isTRUE(msg)) {
+                          private$saved.tick[[Sys.getpid()]] <- as.character(Sys.time())
                         } else {
-                          msg <- as.character(msg)
-                          msg <- sprintf('%s - %s: %s\n', private$saved.tick,
-                                         as.character(Sys.time()), msg)
-                          write.socket(private$log.socket, msg)
-                          private$saved.tick = ''
+                          private$.msg(msg, isInterval = TRUE)
                         }
                       },
                       quit = function() {
@@ -89,6 +92,20 @@ logr <- R6Class("logr",
                         private$log.socket <- NULL
                       }
                     ),
-                    private = list(log.socket = NULL,
-                                   saved.tick = ''),
+                    private = list(
+                      log.socket = NULL,
+                      saved.tick = list(),
+                      .msg = function(msg, withTime = self$withTime, isInterval = FALSE) {
+                          if (length(msg) > 1) warning('length of msg > 1, only the first element is processed.')
+                          fpid <- if (self$withPID) paste0(Sys.getpid(), '\t') else ''
+                          ftime <- if (isInterval) 
+                            sprintf('%s - %s\t', private$saved.tick[[Sys.getpid()]],
+                                             as.character(Sys.time()))
+                          else {
+                            if (withTime) paste0(as.character(Sys.time()), '\t') else ''
+                          }
+                          msg <- sprintf('%s%s%s\n', fpid, ftime, as.character(msg))
+                          write.socket(private$log.socket, msg)
+                      }
+                    )
 )
